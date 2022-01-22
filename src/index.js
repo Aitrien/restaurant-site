fb6485b80fea1e64634c1d65b089b8380ddc5af6
Aitrien <aitriens@gmail.com>
2022-01-22 23:22:40 +1300

    Generated code for header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,70 @@
 import './bootstrap.min.css';
 import './style.css';
 import IceCream from './homeimage.png';
+import Scientist from './scientist.jpg';
 
-const content = document.getElementById('content');
+
+function swapPage(pageName) {
+	console.log(pageName);
+}
+
+function header() {
+	const container = document.createElement('div');
+	container.classList.add('container');
+
+	const head = document.createElement('header');
+	head.classList.add('d-flex', 'flex-wrap', 'justify-content-center', 'py-3', 'mb-4', 'border-bottom');
+
+	const titlelink = document.createElement('a')
+	titlelink.classList.add('d-flex', 'align-items-center', 'mb-3', 'mb-md-0', 'me-md-auto', 'text-dark', 'text-decoration-none');
+
+	const pageTitle = document.createElement('span');
+	pageTitle.classList.add('fs-4');
+	pageTitle.innerText = 'Ice Emporium';
+	titlelink.appendChild(pageTitle);
+	head.appendChild(titlelink);
+
+	const navbar = document.createElement('ul');
+	navbar.classList.add('nav', 'nav-pills');
+
+	// home nav option handled first to add 'active' class
+	const navHome = document.createElement('li');
+	navHome.classList.add('nav-item');
+	const homeLink = document.createElement('a');
+	homeLink.href = "#";
+	homeLink.addEventListener('click', () => swapPage('Home'));
+	homeLink.classList.add('nav-link', 'active');
+	homeLink.textContent = "Home";
+	navHome.appendChild(homeLink)
+	navbar.appendChild(navHome);
+
+	// loop through the rest of the nav pages
+	let tabs = ['About', 'Menu', 'Contact'];
+	tabs.forEach(pageName => {
+		let navPage = document.createElement('li');
+		navPage.classList.add('nav-item');
+		let pageLink = document.createElement('a');
+		pageLink.href = "#";
+		pageLink.addEventListener('click', () => swapPage(pageName));
+		pageLink.classList.add('nav-link');
+		pageLink.innerText = pageName;
+		navPage.appendChild(pageLink);
+		navbar.appendChild(navPage);
+	});
+
+	head.appendChild(navbar);
+	container.appendChild(head);
+
+	return container;
+}
+
+
+// create fixed elements
+document.body.appendChild(header());
+const content = document.createElement('div');
 content.classList.add('container');
+document.body.appendChild(content);
+
 
 // function creates a basic html element and returns it
 function component() {
@@ -21,10 +82,12 @@ function homePage() {
 	container.classList.add('page-container');
 
 	const text = document.createElement('div');
+	text.classList.add('text-container');
 
 	const title = document.createElement('h1');
 	title.classList.add('headline');
-	title.innerText = "Fancy \r\nIce Cream";
+	//title.innerText = "Fancy \r\nIce Cream";
+	title.innerText = "D-LISH Ice Cream";
 
 	const menuLink = document.createElement('a');
 	menuLink.href = "#";
@@ -39,7 +102,7 @@ function homePage() {
 
 	const homeImage = new Image();
   homeImage.src = IceCream;
-	homeImage.classList.add('home-image');
+	homeImage.classList.add('right-image');
 
 	container.appendChild(text);
 	container.appendChild(homeImage);
@@ -47,7 +110,35 @@ function homePage() {
 	return container;
 }
 
+function aboutPage() {
+	const container = document.createElement('div');
+	container.classList.add('page-container');
+
+	const text = document.createElement('div');
+	text.classList.add('text-container');
+
+	const title = document.createElement('h1');
+	title.classList.add('headline');
+	title.innerText = "About you";
+
+	const paragraph = document.createElement('p');
+	paragraph.innerText = "Scientifically proven to deliver sugar!\nOur ethically-sourced ice cream has been determined to leave you wanting more with none of the extra chemical compounds found in other leading brands.";
+
+	text.appendChild(title);
+	text.appendChild(paragraph);
+
+	const aboutImage = new Image();
+  aboutImage.src = Scientist;
+	aboutImage.classList.add('right-image');
+
+	container.appendChild(text);
+	container.appendChild(aboutImage);
+
+	return container;
+}
+
 content.appendChild(homePage());
+//content.appendChild(aboutPage());
 
 //content.appendChild(component());
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Website',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
