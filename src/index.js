bb5c1547cf28ad491b69789727468fadda729b03
Aitrien <aitriens@gmail.com>
2022-01-25 23:15:46 +1300

    Footer added

--- src/index.js
+++ src/index.js
@@ -66,6 +66,23 @@ content.classList.add('container');
 document.body.appendChild(content);
 
 
+function footer() {
+	const foot = document.createElement('footer');
+	foot.id = "sticky-footer"
+	foot.classList.add("py-4", "text-white-50");
+	foot.innerText = "Mock website made by Liam - ";
+
+	const link = document.createElement('a');
+	link.href = "https://github.com/Aitrien";
+	link.innerText = "GitHub";
+
+	foot.appendChild(link);
+
+	return foot;
+}
+
+document.body.appendChild(footer());
+
 function swapPage(pageName) {
 	if (currentPage !== pageName) {
 		// clear elements from content div
@@ -102,17 +119,6 @@ function swapPage(pageName) {
 	}
 
 	console.log(pageName);
-}
-
-// function creates a basic html element and returns it
-function component() {
-	const element = document.createElement('a');
-
-	element.innerHTML = 'Content check';
-	element.href = "#";
-	element.classList.add('btn', 'btn-success');
-
-	return element;
 }
 
 function homePage() {
@@ -223,21 +229,32 @@ function contactPage() {
 	const container = document.createElement('div');
 	container.classList.add('page-container');
 
-	//const 
+	const textSide = document.createElement('div');
+	textSide.classList.add('text-container');
+
+	const title = document.createElement('h1');
+	title.classList.add('contact-headline');
+	title.innerText = "Questions?";
+	textSide.appendChild(title);
+
+	const contacts = document.createElement('p');
+	contacts.innerText = "\nTel: 123-456-7890 \nEmail: info@restaurant.com\nAddress: 123 Fourth Street, Antarctica\n\nOpening Hours: \nMon - Fri: 12pm - 10pm\n​​Saturday: 12pm - 4pm\nSunday: 12pm - 8pm";
+	textSide.appendChild(contacts);
+
+	container.appendChild(textSide);
+
+	const mapSide = document.createElement('div');
 
 	const map = document.createElement('iframe');
 	map.id = "map";
-	map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46396.57488298969!2d172.63187833589654!3d-43.38149993135463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d319167a4416f03%3A0x500ef8684797a90!2sKaiapoi!5e0!3m2!1sen!2snz!4v1643096656325!5m2!1sen!2snz";
+	map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26893.33514137419!2d166.64435820831739!3d-77.84011904210483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xaf773973ada5b34d%3A0xe241f2716549c551!2sMcMurdo%20Station%2C%20Antarctica!5e0!3m2!1sen!2snz!4v1643101999320!5m2!1sen!2snz";
 	map.loading = "lazy";
 	map.allowFullscreen = "";
 	container.appendChild(map);
 
-	return container
+	return container;
 }
 
+
+
 content.appendChild(homePage());
-//content.appendChild(aboutPage());
-
-//content.appendChild(component());
-
-//console.log("pre-flight check");
